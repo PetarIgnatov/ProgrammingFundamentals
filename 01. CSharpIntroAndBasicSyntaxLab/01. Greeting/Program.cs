26eaf7e333f6a0a7c3b8745732c55bc474294f10
ignatov.petar@abv.bg <ignatov.petar@abv.bg>
2023-06-19 22:48:53 +0300

    New

---
+++ 01. CSharpIntroAndBasicSyntaxLab/01. Greeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Greeting
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ 01. CSharpIntroAndBasicSyntaxLab/03. EmployeeData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._EmployeeData
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {id:d8}");
+            Console.WriteLine($"Salary: {salary:f2}");
+            
+            
+        }
+    }
+}
